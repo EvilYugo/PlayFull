137bece1885e68b7d005c372ca3d8ac5ffe1a7b3
EvilYugo <159910790+EvilYugo@users.noreply.github.com>
2024-05-17 11:23:26 +0000

    Alles wieder gut

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const words = [
 ];
 
 const buzzWords = [
-    "Just Do It", "Keep Moving Forward", "To Infinity and Beyond", "Seize the Day", "Stay Hungry, Stay Foolish", 
-    "Think Different", "Imagination is More Important than Knowledge", "The Best is Yet to Come", 
-    "Carpe Diem", "I Have a Dream", "Live Long and Prosper", "May the Force be With You", "I'll Be Back",
-    "To Be or Not to Be", "Elementary, My Dear Watson", "Here's Looking at You, Kid"
+    "Inshallah", "Wallah", "To Infinity and Beyond, brudi", "Shalom & Seize the Day", "Stay Hungry, Stay Inshallah", 
+    "Think Different, wallah", "Imagination is More Important than Knowledge", "The Best is Yet to Come, habibi", 
+    "Carpe Diem, habibi", "I Have a Dream, wallah", "Live Long and Prosper", "May the Force be With You, habibi", "I'll Be Back",
+    "Shalom", "Inshallah, My Dear Watson", "Here's Looking at You, habibi"
 ];
 
 const toggleButton = document.getElementById('toggleButton');
