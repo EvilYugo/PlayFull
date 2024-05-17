e1febeb40c6a4224483de6476937085481f1fb4a
EvilYugo <159910790+EvilYugo@users.noreply.github.com>
2024-05-17 12:09:57 +0000

    Alles wieder gut

--- script.js
+++ script.js
@@ -1,52 +1,38 @@
 // script.js
 
-const words = [
-    "Mein Block", "Gheddo", "Ersguterjunge", "Was Hast Du Gedacht", "Straßenjunge", 
-    "Chabos wissen wer der Babo ist", "Fick dein Money", "Nie ein Rapper", "Frei", 
-    "Kopf kaputt", "Jung, Brutal, Gutaussehend", "Mord", "Asphalt Massaka", "Blyat",
-    "Ich bin 2 Berliner", "Ich und keine Maske", "Niemals Antäuschen", "Badewiese",
-    "Echte Männer", "Egoist"
-];
-
 const buzzWords = [
-    "Straßenkredit", "Mehr Kohle, mehr Probleme", "Baba Saad", "Mein Herz schlägt für Hip Hop", "Deutscher Sprechgesang", 
-    "Rapper aus Deutschland", "Ich bin ein Rapper", "Rap ist mein Leben", 
-    "Untergrund Rap", "Rap ist nicht tot", "Flow auf Deutsch", "Hip Hop Kultur", "Rapmusik für die Straße",
-    "Gangsta Rap", "Deutschrap Anthem", "Hoch die Tassen", "Raptexte voller Bedeutung"
+    "Just Do It", "Keep Moving Forward", "To Infinity and Beyond", "Seize the Day", "Stay Hungry, Stay Foolish", 
+    "Think Different", "Imagination is More Important than Knowledge", "The Best is Yet to Come", 
+    "Carpe Diem", "I Have a Dream", "Live Long and Prosper", "May the Force be With You", "I'll Be Back",
+    "To Be or Not to Be", "Elementary, My Dear Watson", "Here's Looking at You, Kid"
 ];
 
 const toggleButton = document.getElementById('toggleButton');
 const quoteContainer = document.getElementById('quoteContainer');
 let interval;
 let wordCount = 0;
-let quoteIndex = 0;
-const quotes = [
-    "Mama, ich hab' 'nen Porsche gekauft - Bushido",
-    "Das ist Alpha, kein Beta - Kollegah",
-    "Ja, du bist tight, ich bin tighter - Fler",
-    "Stress ohne Grund macht den Puls auf 180 - Farid Bang",
-    "Du bist die Sonne meines Lebens, Baby, doch ich bin ein Mond - Kool Savas"
-];
+let isAnimating = false;
 
 toggleButton.addEventListener('click', () => {
     toggleButton.classList.toggle('clicked');
-    if (interval) {
-        clearInterval(interval);
-        interval = null;
-        showQuote();
-        document.body.style.backgroundColor = 'black'; // Change background color to black
-        removeAllWords(); // Stop displaying words
-        removeGradientAnimation(); // Stop gradient animation
-        toggleButton.textContent = "Start"; // Reset button text
-    } else {
-        startDisplayingWords();
-        toggleButton.textContent = getRandomElement(buzzWords);
+    if (!isAnimating) {
+        isAnimating = true;
+        if (interval) {
+            clearInterval(interval);
+            interval = null;
+            stopAnimations();
+            showQuote();
+            toggleButton.textContent = "Lass düsen";
+        } else {
+            startDisplayingWords();
+            toggleButton.textContent = getRandomElement(buzzWords);
+        }
     }
 });
 
 function startDisplayingWords() {
     interval = setInterval(() => {
-        const word = words[wordCount % words.length];
+        const word = buzzWords[wordCount % buzzWords.length];
         displayWord(word);
         wordCount++;
     }, 2000); // Adjusted display interval
@@ -70,7 +56,7 @@ function createWordElement(word) {
     const element = document.createElement('div');
     element.className = 'word';
     element.textContent = word;
-    element.style.fontSize = `${getRandomInt(40, 200)}px`; // Adjusted size range
+    element.style.fontSize = `${getRandomInt(20, 40)}px`; // Adjusted size range
     element.style.color = getRandomFontColor(); // Random font color
     element.style.fontFamily = getRandomFont(); // Random font family
     element.style.position = 'absolute'; // Position the words absolutely
@@ -79,7 +65,7 @@ function createWordElement(word) {
     return element;
 }
 
-function removeAllWords() {
+function stopAnimations() {
     const words = document.querySelectorAll('.word');
     words.forEach(word => {
         word.style.opacity = 0;
@@ -91,18 +77,14 @@ function removeAllWords() {
     });
 }
 
-function removeGradientAnimation() {
-    document.body.style.animation = 'none';
-}
-
 function showQuote() {
     quoteContainer.classList.remove('hidden');
-    quoteContainer.style.display = 'block';
-    quoteContainer.innerHTML = `<p style="font-size: 5em; color: white;">${quotes[quoteIndex]}</p>`;
-    quoteIndex = (quoteIndex + 1) % quotes.length;
-    toggleButton.style.backgroundColor = 'white'; // Change button background color
-    toggleButton.style.color = 'black'; // Change button text color
+    quoteContainer.innerHTML = getRandomElement(quotes);
     document.body.style.backgroundColor = 'black'; // Change background color to black
+    setTimeout(() => {
+        toggleButton.classList.toggle('clicked');
+        isAnimating = false;
+    }, 1000); // Delay before re-enabling button
 }
 
 function getRandomElement(arr) {
@@ -121,6 +103,6 @@ function getRandomFont() {
 }
 
 function getRandomFontColor() {
-    const colors = ['#000000', '#333333', '#666666', '#999999', '#00008B']; // Black, various shades of gray, dark blue
+    const colors = ['#000000', '#333333', '#666666', '#999999', '#ff8c00']; // Black, various shades of gray, bold orange
     return getRandomElement(colors);
 }
